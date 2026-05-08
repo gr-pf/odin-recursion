3a7e3b4cdf0182e9a14c2db77ae3e10c05bac907
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-05-08 14:19:13 +0200

    Add fibs rec

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {Number} n
+ * @returns {Number[]}
+ */
 export function fibs(n) {
   if (n === 1) {
     return [0];
@@ -10,4 +15,20 @@ export function fibs(n) {
     res.push(res[i - 1] + res[i - 2]);
   }
   return res;
+}
+
+/**
+ *
+ * @param {Number} n
+ * @returns {Number[]}
+ */
+export function fibsRec(n) {
+  if (n === 1) {
+    return [0];
+  }
+  if (n === 2) {
+    return [0, 1];
+  }
+  const rec = fibsRec(n - 1);
+  return rec.concat(rec[rec.length - 1] + rec[rec.length - 2]);
 }
